e92c5c514605715482ca52c2005ec077a8658e24
mishapushka <bskfrost3@yandex.ru>
2021-11-14 23:16:08 +0700

    Initial Commit

---
+++ AnimationProject/AnimationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationProjectApp.swift
+//  AnimationProject
+//
+//  Created by Mikhail Pushkarev on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
